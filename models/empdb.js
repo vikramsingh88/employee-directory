d4d08c57d69c55bdd77348df712e4abcc83e077d
unknown <Vikram.Singh@teamaol.com>
2017-03-13 17:58:12 +0530

    updated db path

--- models/empdb.js
+++ models/empdb.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 //var dbPath = 'mongodb://localhost/empdb';
-var dbPath = 'mongodb://vikram:password@ds161059.mlab.com:61059/empdb';
+var dbPath = 'mongodb://vikram:password@ds161059.mlab.com:61059/chat';
 
 mongoose.connect(dbPath);
 
